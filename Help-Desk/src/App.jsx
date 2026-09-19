13c4c313856637bd01a9011ee8271d10c9169c17
JAINISH729 <jainish729@gmail.com>
2026-09-19 12:56:59 +0530

    uploaded

---
+++ Help-Desk/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import LoginPage from '../pages/Login';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <LoginPage/>
+    </>
+  )
+}
+
+export default App
---
+++ Help-Desk/src/pages/Login.jsx
@@ -0,0 +1,32 @@
+import React,{useState} from 'React';
+
+function Login(){
+
+  const [email,setEmail] = ('useState');
+  const [password,setPassword] = ('useState');
+  const [error,setError] = ('useState');
+}
+const handlesubmit = (e) =>{
+  e.preventDefault();
+  setError();
+  };
+  export default function LoginPage(){
+    return(
+      <div className="container">
+        <div className="row">
+          <div class="mb-3 row">
+  <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+  <div class="col-sm-10">
+    <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
+  </div>
+</div>
+<div class="mb-3 row">
+  <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+  <div class="col-sm-10">
+    <input type="password" class="form-control" id="inputPassword">
+  </div>
+</div>
+        </div>
+      </div>
+      )
+  };
